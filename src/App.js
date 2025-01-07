99a4af7f3d8c37877970dd63e9488ac2656d4464
Javier Andrade <156113956+Xavii57@users.noreply.github.com>
2025-01-07 13:27:15 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+const FollowMause = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
+  }, [enabled]);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          background: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <main>
+      <FollowMause />
+    </main>
+  );
+}
